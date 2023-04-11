40f4ceb70a807fcd62ca42b7269db4216e412516
unknown <deividibaby6@gmail.com>
2023-04-11 11:35:23 -0400

    Feat [ create new router with react-router ]

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import Bulbasaur from './pages/bulbasaur'
+import Charmander from './pages/charmander'
 import Home from './pages/home/index'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Squirtle from './pages/squirtle'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/charmander" element={<Charmander />} />
+        <Route path="/bulbasaur" element={<Bulbasaur />} />
+        <Route path="/squirtle" element={<Squirtle />} />
       </Routes>
     </BrowserRouter>
   )
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -7,9 +7,18 @@ import charmander from '../../assets/charmander.svg'
 import bulbasaur from '../../assets/bulbasaur.svg'
 import squirtle from '../../assets/squirtle.svg'
 import Footer from '../../components/Footer'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
   const [pokelist, setPokelist] = useState()
+  // let Lista = []
+  // useEffect(() => {
+  //   axios
+  //     .get('https://pokeapi.co/api/v2/pokemon/4/')
+  //     .then(res => Lista.push(res.data))
+
+  //   console.log(Lista.indexOf())
+  // }, [])
 
   useEffect(() => {
     axios.get('https://pokeapi.co/api/v2/pokemon').then(res => setPokelist(res))
@@ -19,9 +28,15 @@ export default function Home() {
     <Main>
       <Header title={'PokeGadgets'} />
       <Components>
-        <Card name="Bulbasaur" alt="bulbasaur-image" img={bulbasaur} />
-        <Card name="Charmander" alt="charmeleon-image" img={charmander} $fire />
-        <Card name="Squirtle" alt="squirtle-image" img={squirtle} />
+        <Link to="bulbasaur">
+          <Card name="Bulbasaur" alt="bulbasaur-image" img={bulbasaur} />
+        </Link>
+        <Link to="/charmander">
+          <Card name="Charmander" alt="charmeleon-image" img={charmander} />
+        </Link>
+        <Link to="squirtle">
+          <Card name="Squirtle" alt="squirtle-image" img={squirtle} />
+        </Link>
       </Components>
       <Footer />
     </Main>
