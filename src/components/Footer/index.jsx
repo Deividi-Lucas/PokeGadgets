3306dc245ac3d756dfa09635f2806c5b388033b4
unknown <deividibaby6@gmail.com>
2023-04-11 09:03:18 -0400

    Style [ added footer and styled ]

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,9 @@
+import { Footer_component } from './style'
+
 export default function Footer() {
   return (
-    <div>
+    <Footer_component>
       <h1>Create by Deividi Lucas</h1>
-    </div>
+    </Footer_component>
   )
 }
---
+++ src/components/Footer/style.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+import { grass, dark } from '../../utils/color'
+
+export const Footer_component = styled.footer`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${dark};
+  border-radius: 1rem 1rem 0 0;
+  padding: 0.5rem;
+  color: ${grass};
+  text-shadow: 0 0 1rem black;
+`
